8a1aa29085550275d1f2d2dfa40983eaaccdf3d5
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-23 11:50:28 -0700

    started resized as needed method

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -2,6 +2,22 @@ public class DynamicStringList implements StringList{
     private String[] words;
     private int size;
 
+    public DynamicStringList (){
+        words = new String[10];
+        size = 0;
+    }
+    private void resizeAsNeeded(){
+        if (size == words.length){
+            String [] resizedArray = new String[words.length*2];
+
+        }
+    }
+
+
+
+
+
+
     public String get(int index) {
         
 
