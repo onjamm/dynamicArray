14a0f5764e0fee94d1499c4bc5b5ee317695c7d9
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-27 19:56:48 -0700

    Test set and added before each setup method

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,12 +1,18 @@
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions.*;
 
+
 public class DynamicStringListTest {
+    private DynamicStringList list;
+    @BeforeEach
+    public void setUp() {
+        list = new DynamicStringList();
+    }
     
     @Test
     void testGet(){
         //Arrange
-        DynamicStringList list = new DynamicStringList();
         list.add("word");
 
         //Act
@@ -16,4 +22,17 @@ public class DynamicStringListTest {
         assertEquals("word", actual);
 
     }
+
+    @Test 
+    public void testSet(){
+        list.add("I");
+        list.add("despise");
+        list.add("loud");
+        list.add("cats");
+        list.set(1,"love");
+
+        assertEquals("love", list.get(1));
+    }
+
+
 }
