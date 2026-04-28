dc8cd4042d8bc1ee862f4711b618005e180f0269
onjamm <75151498+onjamm@users.noreply.github.com>
2026-04-27 19:44:10 -0700

    finished testGet

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,19 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions.*;
+
 public class DynamicStringListTest {
     
+    @Test
+    void testGet(){
+        //Arrange
+        DynamicStringList list = new DynamicStringList();
+        list.add("word");
+
+        //Act
+        String actual = list.get(0);
+        
+        //Arrange
+        assertEquals("word", actual);
+
+    }
 }
