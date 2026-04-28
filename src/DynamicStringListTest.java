a1c4de3e9a4e5ba6167c55d13a02ae13be865ecd
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-27 19:58:12 -0700

    added static import

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 
 public class DynamicStringListTest {
